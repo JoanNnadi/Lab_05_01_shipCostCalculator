9b1e9e5e327e012683733a642421d06496126a46
JoanNnadi <nnadijn@mail.uc.edu>
2025-09-20 08:39:06 -0400

    task 1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 import java.util.Scanner;
 
@@ -11,7 +9,37 @@ public class Main
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the price of a shipping item: $ ");
         double itemPrice = in.nextDouble();
-        
+        double shippingCost;
 
-    }
+        if (itemPrice >= 100) {
+            shippingCost = 0;
+            System.out.println( "Your shipping cost is free: ");
+
+        }
+              else
+        {
+            shippingCost = itemPrice * 0.02;
+            double totalPrice = itemPrice + shippingCost;
+            System.out.println("your shippping cost is:  "  + shippingCost);
+            System.out.println("The total shipping price: 15" + totalPrice);
+        }
+
+                if (itemPrice <= 100) {    // when shipping is less than 100
+                    shippingCost = 0;
+
+                    System.out.println(" Your shipping cost is not free: ");
+                }
+
+                else
+                {
+                    shippingCost = itemPrice * 0.02;
+                    double totalPrice = itemPrice + shippingCost;
+                    System.out.println("your shippping cost is:  "  + shippingCost);
+                    System.out.println("The total shipping price: 15" + totalPrice);
+                }
+
+
+
+
+            }
 }
